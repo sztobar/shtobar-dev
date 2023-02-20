94ca3d50cc17b715d36b55d40fc75a7b07ef9f0d
Bartosz Szewczyk <bszewczyk91@gmail.com>
2023-02-20 22:11:27 +0100

    Fix astro page path (#2)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://shtobar-dev.vercel.app',
+  site: 'https://shtobar.dev',
   integrations: [preact(), tailwind({ config: { applyBaseStyles: false } })],
 });
